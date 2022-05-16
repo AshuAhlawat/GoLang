e952ad3dbaaa92e078008d5bcb53c2f8d4c83208
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-05-16 10:01:22 +0530

    Update 05slices.go
    printing out datatypes with arrays and slices

--- 05slices.go
+++ 05slices.go
@@ -10,24 +10,24 @@ func main(){
 	// taking out a slice of the arr
 	var test = arr[2:4]
 
-	fmt.Printf("%v is a Go slice of length %v from the array %v",test,len(test),arr)
+	fmt.Printf("%v is a Go slice of length %v from the array %v of datatype %T",test,len(test),arr,arr)
 
 	test[0] = 5
 	// first param is the slice and it returns a slice with other params appended to its end
 	test = append(test, arr[0], arr[1])
 
-	fmt.Printf("\n%v is our slice\n",test)
+	fmt.Printf("\n%v is our new slice\n",test)
 
 	// extending slices from slices / only one at a time 
 	test = append(test, test[0:2]...)
 
-	fmt.Printf("Our final slice %v of length %v\n\n",test, len(test))
+	fmt.Printf("Our final slice %v of length %v of datatupe %T\n\n",test, len(test), test)
 
 	// as go loads all the items to slice into memory so if the slice is large thats too much data
 	// instead we can copy the items we need into a new array
 
 	numbers := []int{1,2,3,4,5,6,7,8,9,10,11,12,13,14,15}
-	fmt.Printf("Our new slice\n%v\n", numbers)
+	fmt.Printf("Our new slice\n%v of datatype %T\n", numbers,numbers)
 
 	// creating slice of the needed data
 	needed_nums := numbers[0:len(numbers)/2]

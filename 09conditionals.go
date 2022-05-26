ebc01396ed64d8291ffa7d017337c8d8b043bab0
Ashwani Ahlawat <49407895+AshuAhlawat@users.noreply.github.com>
2022-05-26 13:39:54 +0530

    Switches in go

---
+++ 09conditionals.go
@@ -0,0 +1,39 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var num int8 // initialized to 0
+
+	print("Week Day number :")
+	fmt.Scan(&num)
+
+	var day string // initialized to  ""
+
+	switch num{ // starts from curly brackets
+		case 1:
+			day = "Monday"
+		case 2:
+			day = "Tuesday"
+		case 3:
+			day = "Wednesday"
+		case 4:
+			day = "Thursday"
+		case 5:
+			day = "Friday"
+		case 6:
+			day = "Saturday"
+		case 7:
+			day = "Sunday"
+		default: // default case to run if no case is true
+			print("\nThats not a day")
+	
+	}
+
+	if day != "" {
+		print("\nThe day name is ",day)
+	}
+
+
+}
